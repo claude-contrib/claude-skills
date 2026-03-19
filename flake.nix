72b5768d0eb5320ea2c80cd14d50f79e22ea6e79
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-19 15:34:46 +0400

    refactor(flake): restructure outputs and simplify package list
    Reformat the outputs function signature for readability, reorganize package
    declarations to use direct imports instead of withPackages, and reorder
    packages alphabetically.

--- flake.nix
+++ flake.nix
@@ -6,19 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            (python3.withPackages (ps: [ ps.check-jsonschema ]))
             jq
             bash
             shellcheck
+            check-jsonschema
             gh
           ];
         };
-      });
+      }
+    );
 }
